8c616844bb8d79e8378522683a2cf7e80d0ac867
JonSmebye <jonsmebye@gmail.com>
2020-10-12 09:56:46 +0200

    Made Vigenere Cipher page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,11 @@
 import React from "react"
-
 import Layout from "../components/layout"
 
-const NotFoundPage = () => (
+const NotFound = () => (
   <Layout>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
-export default NotFoundPage
+export default NotFound
---
+++ src/pages/vigenereIsAwesome.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Vinegar(){
+    return (
+        <Layout>
+            <h1>Blaise de Vigenère</h1>
+            <img src="https://ciphermachines.com/pictures/vigenere02.jpg" alt="" height="300px" />
+            <p>Blai de Vigenère var blant annet en fransk diplomat, kryptograf og oversetter. Han ble født i byen <a href="https://en.wikipedia.org/wiki/Saint-Pour%C3%A7ain-sur-Sioule" style={{ color:"black" }}>Saint-Pourçain</a> på midten av 1500-tallet. Han tok sin utdanning i Paris, der han studerte språkene gresk, hebraisk og italiensk</p>
+            <p>I 1549 dro Vigenère til Roma på et 2-års langt diplomatisk oppdrag. Her kom han i kontakt med kryptografer og begynte å lese bøker om kryptografi. Selv om Vigenère cipher'en har sitt navn fra Vigenère er dens egentlige oppfinner en helt annen. Den første som beskrev krypteringsmetoden var Italieneren Giovan Battista Bellaso i 1553.</p>
+            <p>Gicromivww lofo nog wevrspsu weoopakdk foqgg hki jsptgb mmi qdvqivit puscrvmfs fedzh wvflog qrpqfv. Ftwqk c gsvkbj yrcq lgf fedzh lqzbwvcqi dzra vvh qcb gsyb vtcbnit Gkmxsu qg hlqdsuw vc js qb dgecxrv zrsmcxx yvhvtm gswposqb flcgh. Fgzdc ac-ks-jc nigzkewz vuwwija poeey vtqh betrdvo gscizdwu gkigh wvcbvso vhexs ws.</p>
+            <p>Fi npcfwi fsq sigde, ood ocbvols eiimqrg o jnqsui fwvwg jdruyhpkuhvg. Bhwvs vxgu impbhv fi xrfsu jcbhr "jjd klspqgf vii w emnrhx?"</p>
+            
+            <div style={{ color:"white" }}>
+                <p>
+                    <a href="https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher" style={{ color:"black" }}>Vigenere</a> er ikke så lett å dekode uten passord, kanskje passordet har noe å gjore med dette arrangementet?
+                </p>
+            </div>
+        </Layout>
+    )
+}
