186081b4e80e037a7a5b68562fc659e08673f7fa
JonSmebye <jonsmebye@gmail.com>
2020-10-14 16:10:47 +0200

    Update vigenere page

--- src/pages/vigenereIsAwesome.js
+++ src/pages/vigenereIsAwesome.js
@@ -6,7 +6,7 @@ export default function Vinegar(){
         <Layout>
             <h1>Blaise de Vigenère</h1>
             <img src="https://ciphermachines.com/pictures/vigenere02.jpg" alt="" height="300px" />
-            <p>Blai de Vigenère var blant annet en fransk diplomat, kryptograf og oversetter. Han ble født i byen <a href="https://en.wikipedia.org/wiki/Saint-Pour%C3%A7ain-sur-Sioule" style={{ color:"black" }}>Saint-Pourçain</a> på midten av 1500-tallet. Han tok sin utdanning i Paris, der han studerte språkene gresk, hebraisk og italiensk</p>
+            <p>Blaise de Vigenère var blant annet en fransk diplomat, kryptograf og oversetter. Han ble født i byen <a href="https://en.wikipedia.org/wiki/Saint-Pour%C3%A7ain-sur-Sioule" style={{ color:"black" }}>Saint-Pourçain</a> på midten av 1500-tallet. Han tok sin utdanning i Paris, der han studerte språkene gresk, hebraisk og italiensk</p>
             <p>I 1549 dro Vigenère til Roma på et 2-års langt diplomatisk oppdrag. Her kom han i kontakt med kryptografer og begynte å lese bøker om kryptografi. Selv om Vigenère cipher'en har sitt navn fra Vigenère er dens egentlige oppfinner en helt annen. Den første som beskrev krypteringsmetoden var Italieneren Giovan Battista Bellaso i 1553.</p>
             <p>Fi npcfwi fsq sigde, ood ocbvols eiimqrg o jnqsui fwvwg jdruyhpkuhvg. Bhwvs vxgu rk fso ip oy jnojkgh impbhv fi så vqa U40.</p>
             <div style={{ color:"white" }}>
